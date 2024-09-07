9ab3efaa833005519c4d7eb5863e6295820aa1e9
roialrogue <144475675+roialrogue@users.noreply.github.com>
2024-09-06 22:03:04 -0400

    Ftc Dashboard setup

---
+++ TeamCode/src/main/java/teamcode/FtcDashboard.java
@@ -0,0 +1,21 @@
+package teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import trclib.robotcore.TrcPidController;
+
+public class FtcDashboard
+{
+    @Config
+    public static class TunePID
+    {
+        public static TrcPidController.PidCoefficients tunePidCoeff = new TrcPidController.PidCoefficients(.10, 0.0, 0.01, 0,0);
+    }
+
+    @Config
+    public static class DriveTuneParams
+    {
+        public static double tuneDistance = 8;
+        public static double tuneAngleDistance = 90;
+        public static double powerLimit = 0.7;
+    }
+}
